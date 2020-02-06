68ac45b5329ae55c9b013d489228c6f5a29a53f8
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-02-06 10:18:10 +0100

    Add files via upload

---
+++ robertJanjaninProsjek/robertJanjaninProsjek/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robertJanjaninProsjek
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            float x;
+            Console.WriteLine("Unesi broj a: ");
+            a=Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Unesi broj b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Unesi broj c: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            x = (a + b + c) / 3;
+            Console.WriteLine("Prosjek 3 broja je: " + x);
+            Console.ReadKey();
+        }
+    }
+}
